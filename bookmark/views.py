0fd2932fba97f123be2a0a7f0525a237a98dce8b
Yun <chlrldbs@gmail.com>
2021-06-06 11:40:33 +0900

    add url / add page

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Bookmark
+
+
+class BookmarkListView(ListView):
+    model = Bookmark
+
+class BookmarkCreateView(CreateView) :
+    model = Bookmark
+    fields = ['site_name','url']
+    success_url = reverse_lazy('list')
+    template_name_suffix =  '_create'
+
+
