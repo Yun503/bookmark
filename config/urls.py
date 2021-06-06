19dbcec93d819d5f1cea3d7ebcea1a176713ec4b
Yun <chlrldbs@gmail.com>
2021-06-06 21:29:29 +0900

    html form add

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('',include('bookmark.urls')),
     path('bookmark/', include('bookmark.urls')),
     path('admin/', admin.site.urls),
 ]
